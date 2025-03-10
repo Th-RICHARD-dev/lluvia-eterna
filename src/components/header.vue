4b183ae7d5ef6aaf09ce29232a62e79b5783b308
basile_drt <115219375+basinho@users.noreply.github.com>
2025-03-10 11:09:52 +0100

    Fixed the issue of the endtags problem (#9)

--- src/components/header.vue
+++ src/components/header.vue
@@ -16,8 +16,16 @@
             <RouterLink to="/shop">
                 <h2>PACKS</h2>
             </RouterLink>
-
         </div>
+    </div>
+    <div class="header">
+        <div class="left">
+            <RouterLink to="/shop"><h2>POPULAIRE</h2></RouterLink> 
+            <RouterLink to="/shop"><h2>BOUTIQUE</h2></RouterLink>
+            <RouterLink to="/product"><h2>PRODUCTIONS</h2></RouterLink>
+            <RouterLink to="/contest"><h2>CONCOURS</h2></RouterLink>
+            <RouterLink to="/shop"><h2>PACKS</h2></RouterLink>
+
         <div class="title">
             <RouterLink to="/">
                 <div v-if="isNotMenu">
