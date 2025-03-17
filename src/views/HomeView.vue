744508a2d806fedcbcbdaca24e9c78631afd7ba4
Thomas Richard <Th.RICHARD2@outlook.com>
2025-03-17 11:44:35 +0100

    final fix tailwind with styling and POC features (#12)
    * fixed tailwind and videos
    
    * removed comments
    
    * final fix tailwind with styling and POC features

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="relative min-h-screen w-full overflow-hidden">
-    <video src="../assets/bg_video.mp4" autoplay loop muted class="absolute top-0 left-0 w-screen h-screen object-fill -z-10"></video>
-    <div class="absolute left-1/2 bottom-[5%] -translate-x-1/2 text-center text-white flex flex-col items-center space-y-2">
+    <video src="../assets/bg_video.webm" autoplay loop muted
+      class="absolute top-0 left-0 w-screen h-screen object-fill -z-10"></video>
+    <div
+      class="absolute left-1/2 bottom-[50px] -translate-x-1/2 text-center text-white flex flex-col items-center">
       <span class="text-[0.75rem] font-bold">4 SAISONS</span>
       <h3 class="text-xs font-light">DECOUVRIR LA COLLECTION</h3>
-      <div class="w-2 h-2 rotate-45 border-r-2 border-b-2 border-white cursor-pointer mt-2"></div>
+      <div class="w-2 h-2 rotate-45 border-r-2 border-b-2 border-white cursor-pointer"></div>
     </div>
   </div>
 </template>
