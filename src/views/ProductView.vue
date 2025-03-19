cb409335a74197282e6b2b0a2c5baddb2686ebeb
Thomas Richard <Th.RICHARD2@outlook.com>
2025-03-19 11:30:39 +0100

    added products and products redirection

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,3 +1,24 @@
+<script>
+import { useRoute } from 'vue-router';
+import data from '@/data/data.json';
+
+export default {
+    setup() {
+        const route = useRoute();
+        const id = route.params.id;
+        const product = data.find((item) => item.id === id);
+
+        return {
+            product,
+        };
+    },
+};
+</script>
+
 <template>
-    
+    <section class="h-screen">
+        <h1 class="text-2xl">Product Name: {{ product.name }}</h1>
+        <p class="text-lg">Description: {{ product.description }}</p>
+        <p class="text-lg">Price: {{ product.price }}</p>
+    </section>
 </template>
