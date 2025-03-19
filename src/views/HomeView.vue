92060d05ea81ae7931b98bd05c85c1ed93591e5d
Clapseur <ganginou27.amorin@gmail.com>
2025-03-19 11:36:50 +0100

    added login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <video src="../assets/bg_video.webm" autoplay loop muted
       class="absolute top-0 left-0 w-screen h-screen object-fill -z-10"></video>
     <div
-      class="absolute left-1/2 bottom-[50px] -translate-x-1/2 text-center text-white flex flex-col items-center">
+      class="absolute left-1/2 bottom-[50px] -translate-x-1/2 text-center text-white flex flex-col items-center animate-pulse">
       <span class="text-[0.75rem] font-bold">4 SAISONS</span>
       <h3 class="text-xs font-light">DECOUVRIR LA COLLECTION</h3>
       <div class="w-2 h-2 rotate-45 border-r-2 border-b-2 border-white cursor-pointer"></div>
