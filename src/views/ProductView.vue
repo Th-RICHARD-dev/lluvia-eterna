ed3e05a336024d7e010306b796e5ec43fbd81f41
basinho <duartebasile6@gmail.com>
2025-04-02 00:17:39 +0200

    updated shop page with products and started the product page

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,1 +1,20 @@
-<template></template>
+<template>
+     <section class="h-screen flex justify-center items-end mt-[10vh] mb-[10vh] bg-[#aaa396]">
+        <div class="absolute left-1/2 bottom-[50px] -translate-x-1/2 text-center text-black flex flex-col items-center">
+            <h3 class="text-[0.75rem] font-bold uppercase">Produit du jour</h3>
+            <p class="text-xs font-light uppercase">Explorer</p>
+        </div>
+    </section>
+    <section class="h-[180vh] justify-center place-items-center ">
+        <div class="h-[50%] w-[85%] mb-4 pt-30 pl-20 bg-[#d9d9d9] rounded-2xl flex flex-row gap-5">
+            <div class="h-[80%] w-[55%] bg-[#b59e7d] rounded-2xl flex justify-center">
+               <img src="../assets/summer_product.svg" class="my-5"> 
+            </div>
+            <p class="w-[37%] font-playfair uppercase">
+            Les premiers rayons de soleil caressent la peau, l’air s’emplit de parfums frais et fleuris… PRINTEMPS est une parenthèse olfactive, un écho aux souvenirs de renaissance et d’insouciance, où chaque senteur réveille une émotion douce et lumineuse.
+            Dès les premières notes, un éclat d’agrumes – citron vert, bergamote et mandarine – apporte une fraîcheur pétillante, rappelant la rosée du matin sur les jeunes bourgeons. Puis, un cœur fleuri de jasmin, de muguet et de fleur d’oranger s’épanouit, évoquant les jardins en éveil et les promenades sous les cerisiers en fleurs. Enfin, un fond délicat de musc blanc et de bois tendres prolonge cette sensation de pureté, tel un vent doux emportant les souvenirs d’une saison naissante.
+            PRINTEMPS, c’est le parfum du renouveau, des premiers jours lumineux, des éclats de rire légers sous un ciel clair, une promesse d’instants heureux à venir.
+            </p>
+        </div>
+    </section>
+</template>
