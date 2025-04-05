bd1055fd06dcafb83e55a9987a6f1ad993b7a495
basinho <duartebasile6@gmail.com>
2025-04-05 14:45:28 +0200

    can now display the correct product page when click on the product in the shop page

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,20 +1,49 @@
 <template>
-     <section class="h-screen flex justify-center items-end mt-[10vh] mb-[10vh] bg-[#aaa396]">
+    <section class="h-screen flex justify-center items-end mt-[10vh] mb-[10vh] bg-[#aaa396]">
         <div class="absolute left-1/2 bottom-[50px] -translate-x-1/2 text-center text-black flex flex-col items-center">
             <h3 class="text-[0.75rem] font-bold uppercase">Produit du jour</h3>
             <p class="text-xs font-light uppercase">Explorer</p>
         </div>
     </section>
-    <section class="h-[180vh] justify-center place-items-center ">
-        <div class="h-[50%] w-[85%] mb-4 pt-30 pl-20 bg-[#d9d9d9] rounded-2xl flex flex-row gap-5">
-            <div class="h-[80%] w-[55%] bg-[#b59e7d] rounded-2xl flex justify-center">
-               <img src="../assets/summer_product.svg" class="my-5"> 
+    <section class="h-[180vh] flex justify-center items-center">
+        <div class="h-[50%] w-[85%] mb-4 bg-[#d9d9d9] rounded-2xl flex flex-row gap-10 p-5">
+            <!-- Golden square -->
+            <div class="h-[80%] w-[50%] mt-20 ml-10 bg-[#b59e7d] rounded-2xl flex flex-col justify-between items-center gap-2 p-5">
+                <h2 class="text-[1.5em] font-bold text-center uppercase">{{product.nom}}</h2>
+                <img :src="product.image" class="w-[61%] h-[75%] rounded-2xl">
+                <h2 class="text-[1.5em] font-bold text-center uppercase">69.99€</h2>
             </div>
-            <p class="w-[37%] font-playfair uppercase">
-            Les premiers rayons de soleil caressent la peau, l’air s’emplit de parfums frais et fleuris… PRINTEMPS est une parenthèse olfactive, un écho aux souvenirs de renaissance et d’insouciance, où chaque senteur réveille une émotion douce et lumineuse.
-            Dès les premières notes, un éclat d’agrumes – citron vert, bergamote et mandarine – apporte une fraîcheur pétillante, rappelant la rosée du matin sur les jeunes bourgeons. Puis, un cœur fleuri de jasmin, de muguet et de fleur d’oranger s’épanouit, évoquant les jardins en éveil et les promenades sous les cerisiers en fleurs. Enfin, un fond délicat de musc blanc et de bois tendres prolonge cette sensation de pureté, tel un vent doux emportant les souvenirs d’une saison naissante.
-            PRINTEMPS, c’est le parfum du renouveau, des premiers jours lumineux, des éclats de rire légers sous un ciel clair, une promesse d’instants heureux à venir.
-            </p>
+            <!-- Description section -->
+            <div class="flex flex-col justify-start items-center w-[37%] mt-25 gap-5">
+                <h2 class="text-[1.5em] font-bold uppercase">Description</h2>
+                <p class="uppercase">
+                    {{product.description}}<br>
+                </p>
+                <div class="flex flex-row justify-between items-center w-full mt-20">
+                    <div class="h-[7.5vh] w-[20%] mb-4 bg-[#969696] rounded-2xl flex justify-center items-center">
+                        <p class="text-center text-[1.25rem] font-bold uppercase text-white">50 ml</p>
+                    </div>
+                    <div class="h-[7.5vh] w-[20%] mb-4 bg-[#969696] rounded-2xl flex justify-center items-center">
+                        <p class="text-center text-[1.25rem] font-bold uppercase text-white">100 ml</p>
+                    </div>
+                    <div class="h-[7.5vh] w-[20%] mb-4 bg-[#969696] rounded-2xl flex justify-center items-center">
+                        <p class="text-center text-[1.25rem] font-bold uppercase text-white">150 ml</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
+<script setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import products from '@/assets/products.json' // importe ton JSON
+
+const route = useRoute()
+const id = computed(() => route.params.id)
+
+// Trouve le bon produit
+const product = computed(() =>
+  products.find(p => p.id === id.value)
+)
+</script>
