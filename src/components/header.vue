23b5cd8a7462bd4e2ec124f27f066d09a599fd10
Thomas Richard <Th.RICHARD2@outlook.com>
2025-04-18 14:16:57 +0200

    refactor: update header component styles and structure

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
-    <div :class="['header', { 'white-header': isNotMenu }]">
-        <div class="left">
+    <div :class="[isNotMenu ? 'bg-[#F1EADA] text-black' : 'text-white', 'z-100 fixed top-0 w-full flex justify-between items-center px-4 py-2 h-[10vh]']">
+        <div class="flex justify-around items-center text-base w-full gap-4 px-10">
             <RouterLink to="/shop">
                 <h2>BOUTIQUE</h2>
             </RouterLink>
@@ -14,17 +14,17 @@
                 <h2>PACKS</h2>
             </RouterLink>
         </div>
-        <div class="title">
+        <div class="flex justify-center items-center text-sm w-2/5 mx-5">
             <RouterLink to="/">
                 <div v-if="isNotMenu">
-                    <img class="img" src="../assets/logo_black.svg" alt="black logo">
+                    <img class="w-24 h-auto" src="../assets/logo_black.svg" alt="black logo">
                 </div>
                 <div v-else>
-                    <img class="img" src="../assets/logo.svg">
+                    <img class="w-24 h-auto" src="../assets/logo.svg">
                 </div>
             </RouterLink>
         </div>
-        <div class="right">
+        <div class="flex justify-around items-center text-base w-full gap-4 px-10">
             <RouterLink to="/about">
                 <h2>HISTOIRE</h2>
             </RouterLink>
@@ -32,17 +32,14 @@
                 <h2>SERVICE CLIENT</h2>
             </RouterLink>
             
-            <!-- Login button or profile icon based on auth state -->
             <RouterLink v-if="!user" to="/signup">
                 <h2>SE CONNECTER</h2>
             </RouterLink>
             <RouterLink v-else to="/account">
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                    :class="['profile-icon', { 'black-icon': isNotMenu }]" 
+                    :class="[isNotMenu ? 'text-black' : 'text-white', 'w-5 h-5']" 
                     viewBox="0 0 24 24" 
-                    fill="currentColor"
-                    width="24" 
-                    height="24">
+                    fill="currentColor">
                     <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
                 </svg>
             </RouterLink>
@@ -68,7 +65,6 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 
 export default {
@@ -83,85 +79,12 @@ export default {
         }
     },
     async mounted() {
-        // Get initial user state
         const { data: { user } } = await supabase.auth.getUser()
         this.user = user
 
-        // Listen for auth changes
         supabase.auth.onAuthStateChange((_, session) => {
             this.user = session?.user || null
         })
     }
 }
 </script>
-
-<style scoped>
-.header {
-    z-index: 100;
-    position: fixed;
-    top: 0;
-    width: 100vw;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #ffffff;
-    padding: 15px;
-    height: 10vh;
-}
-
-.white-header {
-    background-color: #F1EADA;
-    color: #000000;
-}
-
-.left,
-.right {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    font-size: 1em;
-    width: 100%;
-    gap: 15px;
-    padding: 0 40px;
-}
-
-.title {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 0.75em;
-    width: 40%;
-    margin: 0 20px;
-}
-
-.img {
-    width: 100px;
-    height: auto;
-}
-
-.profile-icon {
-    color: white;
-    width: 20px;
-    height: 20px;
-}
-
-.black-icon {
-    color: black;
-}
-
-@media screen and (max-width: 1060px) {
-    .left,
-    .right {
-        font-size: 0.35em;
-    }
-
-    .title {
-        font-size: 0.5em;
-    }
-
-    .profile-icon {
-        width: 16px;
-        height: 16px;
-    }
-}
-</style>
