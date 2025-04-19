6640c29bfc1796e052455c26bc8e00314ef706d6
Thomas Richard <Th.RICHARD2@outlook.com>
2025-04-19 13:53:02 +0200

    Refactor code structure for improved readability and maintainability

--- src/views/Production.vue
+++ src/views/Production.vue
@@ -1,3 +1,24 @@
 <template>
-  
+    <img class="h-full" src="../assets/production_header_img.svg" alt="production header">
+    <section class="h-[100vh] flex flex-col justify-center items-center text-center">
+        <h3 class="uppercase font-semibold my-4 text-[#584738]">Lluvia eterna PRODUCTIONS</h3>
+        <p class="text-[#584738]">lluvia eterna Productions is proud to announce three long featured films in competition in the .... official
+            selection of<br>
+            the ..th edition of the Cannes Film Festival: «.......» by ........, «.....» by ......... and<br>
+            «..........» by ......... .<br>
+        </p>
+        <p class="text-[#584738]">lluvia eterna Productions is a registered subsidiary of the house, marks the first house to count the full-<br>
+            fledged production of films among its activities. The division, envisioned by .......... for lluvia eterna,
+            is in<br>
+            line with ........ assured steering of the brand into the future, while echoing the cinematic breadth and
+            nuances of<br>
+            his collections.
+        </p>
+    </section>
+
+    <section class="h-[275vh] flex flex-col justify-around items-center text-center mb-40">
+        <img class="h-[500px]" src="../assets/production_collab_img.svg" alt="collab 1">
+        <img class="h-[500px]" src="../assets/production_collab_img_2.svg" alt="collab 2">
+        <img class="h-[500px]" src="../assets/production_collab_img_3.svg" alt="collab 3">
+    </section>
 </template>
