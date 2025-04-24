2eefdc0a0b2d313aa207f3f26a381750a75a3414
Thomas Richard <Th.RICHARD2@outlook.com>
2025-04-24 12:52:02 +0200

    fix: replace div with RouterLink for improved navigation in Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div class="relative min-h-screen w-full overflow-hidden">
     <video src="../assets/bg_video.webm" autoplay loop muted
       class="absolute top-0 left-0 w-screen h-screen object-fill -z-10"></video>
-    <div
+    <RouterLink to="/shop"
       class="absolute left-1/2 bottom-[50px] -translate-x-1/2 text-center text-white flex flex-col items-center animate-pulse">
-      <span class="text-[0.75rem] font-bold uppercase">4 saisons</span>
-      <h3 class="text-xs font-light uppercase">Découvrir la collection</h3>
-      <div class="w-2 h-2 rotate-45 border-r-2 border-b-2 border-white cursor-pointer"></div>
-    </div>
+        <span class="text-[0.75rem] font-bold uppercase">4 saisons</span>
+        <h3 class="text-xs font-light uppercase">Découvrir la collection</h3>
+        <div class="w-2 h-2 rotate-45 border-r-2 border-b-2 border-white"></div>
+    </RouterLink>
   </div>
 </template>
