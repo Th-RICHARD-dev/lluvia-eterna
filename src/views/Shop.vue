2b5a4d62f09a7315badaae39e6493fc85509125f
Thomas Richard <Th.RICHARD2@outlook.com>
2025-05-12 13:31:21 +0200

    refactor Shop.vue layout and improve responsiveness of product sections

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -33,21 +33,23 @@ export default {
 </script>
 
 <template>
-    <section class="h-[75%] flex justify-center mt-[10vh] mb-[10vh] bg-[#B59E7D]">
-        <swiper :spaceBetween="30" :autoplay="{delay: 2500, disableOnInteraction: false,}" :navigation="true" :centeredSlides="true" :modules="modules" class="mySwiper">
+    <section class="h-[80vh] flex justify-center mt-[10vh] mb-[10vh] bg-[#B59E7D]">
+        <swiper :spaceBetween="30" :navigation="true" :centeredSlides="true" :modules="modules" class="mySwiper">
             <swiper-slide>
-                <div class="flex flex-row justify-center items-center h-full gap-10">
-                    <img src="../assets/leather_product_big.svg" class="w-[40%] max-h-[75%] rounded-2xl ml-5 md:ml-20 ">
-                    <div class="flex flex-col justify-start items-start mr-10 lg:mr-40">
-                        <h1 class="text-[2.5em] text-[#584738] font-bold">Nouveautés :</h1>
-                        <h1 class="text-[2.5em] text-[#584738] font-bold">Parfums d'été 2025</h1>
-                        <p class="text-[1.25em] text-[#584738]">
+                <div class="h-full flex justify-center items-center p-10 gap-10">
+                    <div>
+                        <img src="../assets/leather_product_big.svg" class="rounded-2xl">
+                    </div>
+                    <div>
+                        <h1 class="md:text-[2.5em] text-[1.5em] text-[#584738] font-bold">Nouveautés :</h1>
+                        <h1 class="md:text-[2.5em] text-[1.5em] text-[#584738] font-bold">Parfums d'été 2025</h1>
+                        <p class="md:text-[1.25em] text-[0.75em] text-[#584738]">
                             Découvrez toutes les nouveautés des collections automne et été. Laisser vous émerger en vous
                             vos souvenirs préférer à travers ces nouvelles odeurs.
                         </p>
-                        <a href="#collections" class="flex justify-center w-full mt-5 lg:mt-20">
+                        <a href="#collections">
                             <button
-                                class="bg-[#B59E7D] text-[#584738] border border-[#584738] rounded-xl px-6 py-2 text-[1.25em] hover:bg-[#584738] hover:text-white transition duration-300">
+                                class="bg-[#B59E7D] text-[#584738] border border-[#584738] rounded-xl px-6 py-2 md:text-[1.25em] text-[0.75em] hover:bg-[#584738] hover:text-white transition duration-300">
                                 Découvrir
                             </button>
                         </a>
@@ -55,58 +57,58 @@ export default {
                 </div>
             </swiper-slide>
             <swiper-slide>
-                <div class="flex flex-row justify-center items-center h-full gap-10">
-                    <div class="w-[50%] flex flex-col justify-start items-start ml-20">
-                        <h1 class="text-[2.5em] text-[#584738] font-bold">De nouveau en stock :</h1>
-                        <h1 class="text-[2.5em] text-[#584738] font-bold">Collection été 2024</h1>
-                        <p class="text-[1.25em] text-[#584738]">
+                <div class="h-full flex justify-center items-center p-10 gap-10">
+                    <div>
+                        <h1 class="md:text-[2.5em] text-[1.5em] text-[#584738] font-bold">De nouveau en stock :</h1>
+                        <h1 class="md:text-[2.5em] text-[1.5em] text-[#584738] font-bold">Collection été 2024</h1>
+                        <p class="md:text-[1.25em] text-[0.75em] text-[#584738]">
                             Laissez vous transporter dans le temps ! Retrouver le calme et vos souvenirs heureux de vos
                             vacances d’été au bord de mer grâce à ces douces odeurs.
                         </p>
                     </div>
-                    <div class="w-[20%] h-[45%] bg-[#b59e7d] rounded-2xl flex flex-col items-center p-4 gap-4">
-                        <img src="../assets/summer_product_big.svg" class="w-[95%] h-auto rounded-2xl">
-                        <a @click="handleSquareClick(1)"
-                            class="w-[60%] bg-[#B59E7D] text-center text-[#584738] border border-[#584738] rounded-xl px-6 py-2 text-[1.25em] hover:bg-[#584738] hover:text-white transition duration-300">
-                            <button>
-                                Découvrir
-                            </button>
-                        </a>
-                    </div>
-                    <div class="w-[20%] h-[45%] bg-[#b59e7d] rounded-2xl flex flex-col items-center p-4 gap-4">
-                        <img src="../assets/sea_product_big.svg" class="w-[95%] h-auto rounded-2xl">
-                        <a @click="handleSquareClick(7)"
-                            class="w-[60%] bg-[#B59E7D] text-center text-[#584738] border border-[#584738] rounded-xl px-6 py-2 text-[1.25em] hover:bg-[#584738] hover:text-white transition duration-300">
-                            <button>
-                                Découvrir
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </swiper-slide>
-            <swiper-slide>
-                <div class="flex flex-row justify-center items-center h-full gap-80">
-                    <div class="w-[75%] flex flex-col justify-start items-start ml-20">
-                        <h1 class="text-[2.5em] text-[#584738] font-bold">Offrez</h1>
-                        <h1 class="text-[2.5em] text-[#584738] font-bold">Une jolie surprise !</h1>
-                        <p class="text-[1.25em] text-[#584738]">
-                            Venez découvrir nos packs cadeaux pour offrir un beau parfum à vos proches, vos amis !
-                        </p>
-                        <div class="flex justify-center w-full mt-20">
-                            <a href="./Gift.vue">
-                                <button
-                                    class="bg-[#B59E7D] text-[#584738] border border-[#584738] rounded-xl px-6 py-2 text-[1.25em] hover:bg-[#584738] hover:text-white transition duration-300">
+                    <div class="h-full flex md:flex-row flex-col gap-10">
+                        <div class="flex flex-col md:justify-end items-center gap-5">
+                            <img src="../assets/summer_product_big.svg" class="rounded-2xl">
+                            <a @click="handleSquareClick(1)"
+                                class="bg-[#B59E7D] text-center text-[#584738] border border-[#584738] rounded-xl px-6 py-2 md:text-[1.25em] text-[0.75em] hover:bg-[#584738] hover:text-white transition duration-300">
+                                <button>
+                                    Découvrir
+                                </button>
+                            </a>
+                        </div>
+                        <div class="flex flex-col md:justify-start items-center gap-5">
+                            <img src="../assets/sea_product_big.svg" class="rounded-2xl">
+                            <a @click="handleSquareClick(7)"
+                                class="bg-[#B59E7D] text-center text-[#584738] border border-[#584738] rounded-xl px-6 py-2 md:text-[1.25em] text-[0.75em] hover:bg-[#584738] hover:text-white transition duration-300">
+                                <button>
                                     Découvrir
                                 </button>
                             </a>
                         </div>
                     </div>
-
-                    <div class="w-[50%] h-[75%] bg-[#b59e7d] flex justify-center items-center mt-10">
-                        <img src="../assets/box_winter.svg"
-                            class="w-[50%] h-[40%] rounded-2xl absolute z-10 mt-15 mr-130" />
-                        <div class="relative z-5">
-                            <img src="../assets/winter_product_big.svg" class="w-[80%] h-[90%] rounded-2xl" />
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="h-full flex justify-center items-center p-10 gap-10">
+                    <div>
+                        <h1 class="md:text-[2.5em] text-[1.5em] text-[#584738] font-bold">Offrez</h1>
+                        <h1 class="md:text-[2.5em] text-[1.5em] text-[#584738] font-bold">Une jolie surprise !</h1>
+                        <p class="md:text-[1.25em] text-[0.75em] text-[#584738]">
+                            Venez découvrir nos packs cadeaux pour offrir un beau parfum à vos proches, vos amis !
+                        </p>
+                        <a href="./Gift.vue">
+                            <button
+                                class="mt-5 bg-[#B59E7D] text-[#584738] border border-[#584738] rounded-xl px-6 py-2 md:text-[1.25em] text-[0.75em] hover:bg-[#584738] hover:text-white transition duration-300">
+                                Découvrir
+                            </button>
+                        </a>
+                    </div>
+                    <div class="h-full flex md:flex-row flex-col gap-10">
+                        <div class="flex flex-col md:justify-end items-center gap-5">
+                            <img src="../assets/box_winter.svg" class="rounded-2xl">
+                        </div>
+                        <div class="flex flex-col md:justify-start items-center gap-5">
+                            <img src="../assets/winter_product_big.svg" class="rounded-2xl">
                         </div>
                     </div>
                 </div>
